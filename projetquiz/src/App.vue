5bda00a929c05c722a33e9eb18e583251ba07774
AnthonyGardelle <a.gardelle18@gmail.com>
2024-03-27 17:02:38 +0100

    maj

--- projetquiz/src/App.vue
+++ projetquiz/src/App.vue
@@ -1,30 +1,62 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <div class="card bg-light mb-3">
+      <h1>Les Questionnaires</h1>
+      <div class="card" v-for="questionnaire in questionnaires.questionnaires" :key="questionnaire.id">
+        <QuestionnaireItem :questionnaire="questionnaire" @remove="removeQuestionnaire" @modify="editQuestionnaire"
+          @consult="consultQuestionnaire" />
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import QuestionnaireItem from './components/QuestionnaireItem.vue';
+
+export default {
+  components: {
+    QuestionnaireItem
+  },
+  data() {
+    return {
+      questionnaires: []
+    };
+  },
+  mounted() {
+    this.fetchQuestionnaire();
+  },
+  methods: {
+    fetchQuestionnaire() {
+      fetch("http://127.0.0.1:5000/quiz/api/v1.0/questionnaires")
+        .then(response => response.json())
+        .then(data => {
+          this.questionnaires = data;
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+    },
+    removeQuestionnaire(payload) {
+      const index = this.questionnaires.questionnaires.findIndex(questionnaire => questionnaire.id === payload.id);
+      if (index !== -1) {
+        this.questionnaires.questionnaires.splice(index, 1);
+      }
+    },
+    editQuestionnaire(payload) {
+      let newName = prompt("Entrez le nouveau nom du questionnaire");
+      let index = this.questionnaires.questionnaires.findIndex(questionnaire => questionnaire.id === payload.id);
+      if (index !== -1 && newName !== null && newName !== "") {
+        this.questionnaires.questionnaires[index].name = newName;
+      }
+    },
+    consultQuestionnaire(payload) {
+      let index = this.questionnaires.questionnaires.findIndex(questionnaire => questionnaire.id === payload.id);
+      if (index !== -1) {
+        alert(this.questionnaires.questionnaires[index].name);
+        alert(this.questionnaires.questionnaires[index].questions);
+      }
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
---
+++ projetquiz/src/components/QuestionnaireItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="d-flex justify-content-around align-items-center">
+        <span style="width: 50%;">{{ questionnaire.name }}</span>
+        <input type="button" class="btn btn-danger" value="Supprimer" @click="suppr">
+        <input type="button" class="btn btn-warning" value="Modifier" @click="modif">
+        <input type="button" class="btn btn-primary" value="Consulter" @click="consulter">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        questionnaire: Object
+    },
+    methods: {
+        suppr() {
+            this.$emit('remove', { id: this.questionnaire.id });
+        },
+        modif() {
+            this.$emit('modify', { id: this.questionnaire.id });
+        },
+        consulter() {
+            this.$emit('consult', { id: this.questionnaire.id });
+        }
+    },
+    emits: ['remove', 'modify', 'consult']
+}
+</script>
