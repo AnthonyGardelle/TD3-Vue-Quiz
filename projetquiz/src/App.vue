cae540fd19d10618849cb40ee2ad73d29fe9f586
AnthonyGardelle <a.gardelle18@gmail.com>
2024-04-03 17:06:31 +0200

    maj à compléter

--- projetquiz/src/App.vue
+++ projetquiz/src/App.vue
@@ -5,7 +5,7 @@
       <h1>Les Questionnaires</h1>
       <div class="card" v-for="questionnaire in questionnaires.questionnaires" :key="questionnaire.id">
         <QuestionnaireItem :questionnaire="questionnaire" @remove="removeQuestionnaire" @modify="editQuestionnaire"
-          @consult="consultQuestions" @remove-q="removeQuestion" />
+          @consult="consultQuestions" @removeQ="removeQuestion" @addQ="addQuestion"/>
       </div>
     </div>
   </div>
@@ -41,13 +41,37 @@ export default {
       const index = this.questionnaires.questionnaires.findIndex(questionnaire => questionnaire.id === payload.id);
       if (index !== -1) {
         this.questionnaires.questionnaires.splice(index, 1);
+        fetch(`http://127.0.0.1:5000/quiz/api/v1.0/questionnaires/${payload.id}`, {
+          method: 'DELETE'
+        }).then(response => response.json())
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
       }
     },
     editQuestionnaire(payload) {
       let newName = prompt("Entrez le nouveau nom du questionnaire");
       let index = this.questionnaires.questionnaires.findIndex(questionnaire => questionnaire.id === payload.id);
-      if (index !== -1 && newName !== null && newName !== "") {
+      if (index !== -1 && newName !== null && newName !== "" && newName.trim() !== "") {
         this.questionnaires.questionnaires[index].name = newName;
+        fetch(`http://127.0.0.1:5000/quiz/api/v1.0/questionnaires/${payload.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: newName
+          })
+        }).then(response => response.json())
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
       }
     },
     consultQuestions(payload) {
@@ -64,7 +88,41 @@ export default {
       }
     },
     removeQuestion(payload) {
-      console.log(payload.id);
+      let index = this.questionnaires.questionnaires.findIndex(questionnaire => questionnaire.id === payload.id);
+      let questionIndex = this.questionnaires.questionnaires[index].questions.findIndex(question => question.id === payload.questionId);
+      if (questionIndex !== -1) {
+        this.questionnaires.questionnaires[index].questions.splice(questionIndex, 1);
+        fetch(`http://127.0.0.1:5000/quiz/api/v1.0/questions/${payload.questionId}`, {
+          method: 'DELETE'
+        }).then(response => response.json())
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+      }
+    },
+    addQuestion(payload) {
+      let div = document.getElementById(`ajouterQuestion${payload.id}`);
+      let bouton = document.getElementById("addQ-" + payload.id);
+      let radioSimple = document.getElementById("simple" + payload.id);
+      let radiomultiple = document.getElementById("multiple" + payload.id);
+      radioSimple.addEventListener("click", function() {
+        document.getElementById("choix3").style.display = "none";
+        document.getElementById("choix4").style.display = "none";
+      });
+      radiomultiple.addEventListener("click", function() {
+        document.getElementById("choix3").style.display = "block";
+        document.getElementById("choix4").style.display = "block";
+      });  
+      if (div.style.display === "none") {
+        div.style.display = "block";
+        bouton.value = "Fermer";
+      } else {
+        div.style.display = "none";
+        bouton.value = "Ajouter Question";
+      }
     }
   }
 }
--- projetquiz/src/components/QuestionnaireItem.vue
+++ projetquiz/src/components/QuestionnaireItem.vue
@@ -9,14 +9,32 @@
                     <input type="radio" :name="`question-${question.id}`" :value="question.question.choix1">
                     <input type="radio" :name="`question-${question.id}`" :value="question.question.choix2">
                     <label>{{ question.question.choix2 }}</label>
-                    <input type="button" class="btn btn-danger" value="Supprimer" @click="supprQ">
+                    <input type="button" class="btn btn-danger" value="Supprimer" @click="supprQ(question.id)">
                 </div>
                 <div v-else-if="question.question_type === 'multiple'" class="d-flex justify-content-around">
                     <div v-for="(choice, index) in question.question" :key="index">
                         <label>{{ choice }}</label>
                         <input type="checkbox" :name="`question-${question.id}`" :value="choice">
                     </div>
-                    <input type="button" class="btn btn-danger" value="Supprimer" @click="supprQ">
+                    <input type="button" class="btn btn-danger" value="Supprimer" @click="supprQ(question.id)">
+                </div>
+            </div>
+            <input type="button" class="btn btn-primary" value="Ajouter Question" @click="addQ"
+                :id="`addQ-${questionnaire.id}`">
+            <div :id="`ajouterQuestion-${questionnaire.id}`" style="display: none;">
+                <form action="">
+                    <input type="radio" name="typeQ" :id="``" checked>
+                    <label for="simple">Simple</label>
+                    <input type="radio" name="typeQ" id="multiple">
+                    <label for="multiple">Multiple</label>
+                </form>
+                <div class="d-flex flex-wrap justify-content-center"
+                    style="width: 10%; margin-left: auto; margin-right: auto;">
+                    <input type="text" id="questionTitle" placeholder="Question">
+                    <input type="text" id="choix1" placeholder="Choix 1">
+                    <input type="text" id="choix2" placeholder="Choix 2">
+                    <input type="text" id="choix3" placeholder="Choix 3">
+                    <input type="text" id="choix4" placeholder="Choix 4">
                 </div>
             </div>
         </div>
@@ -49,10 +67,13 @@ export default {
         consult() {
             this.$emit('consult', { id: this.questionnaire.id });
         },
-        supprQ() {
-            this.$emit('removeQ', { id: this.questionnaire.id });
+        supprQ(questionId) {
+            this.$emit('removeQ', { id: this.questionnaire.id, questionId: questionId });
+        },
+        addQ() {
+            this.$emit('addQ', { id: this.questionnaire.id });
         }
     },
-    emits: ['remove', 'modify', 'consult', 'removeQ']
+    emits: ['remove', 'modify', 'consult', 'removeQ', "addQ"]
 }
 </script>
